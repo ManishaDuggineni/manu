645b9f5522f23b4eb26725261adeb2b3711627ac
admin <admin@admin-PC>
2017-11-16 17:08:07 +0530

    new files sendg second commit

---
+++ WebPrjct4/src/VisitCounter.java
@@ -0,0 +1,27 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/VisitCounter")
+public class VisitCounter extends HttpServlet {
+	int i=0;
+	public void init()
+	{
+		i=1;
+	}
+	 protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		out.println(i);
+		i++;
+		
+	}
+
+}
